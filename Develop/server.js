5006bf05add2ee02875538b12e532d9ba8c32cf4
Samantha Shuttleworth <sammi25@Samanthas-Air.attlocal.net>
2024-05-01 22:20:20 -0500

    added routes, middleware, and added a code if an error occured

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,30 @@
 //Note: Starter code for getting the server set up using express.js
 const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//ToDO: Set up routes
+//Note: parse the url encoded bodies (sent by html forms)
+app.use(express.urlencoded({
+    extended: true
+}));
 
+//Note: parse JSON bodies (sent by API clients)
+app.use(express.json());
 
-//ToDo: Set up middleware
+//Note: Middleware (static files from 'public directory)
+app.use(express.static('public'));
 
+//Note: Defining html and api routes
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
 
-//ToDo: Set up configurations
-
+//Note: Error handling middleware (if it is needed)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Looks like something broke!');
+});
 
 //Note: Starting the express server (listen for incoming http requests through this port)
 app.listen(PORT, () => {
